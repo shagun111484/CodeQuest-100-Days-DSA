223e17be2612553600fed27d4814950812d129e4
shagun111484 <shagunsingh3290@gmail.com>
2025-04-14 14:30:21 +0530

    day_14_restore.py

---
+++ submissions/shagun_singh/day_14_restore.py
@@ -0,0 +1,16 @@
+def restore_string(s):
+    result = []
+    for i in range(len(s)):
+        if s[i] == '_':  
+            # Pichle letter ki ASCII value lo aur ek bada karo
+            missing_char = chr(ord(s[i-1]) + 1)
+            result.append(missing_char)
+        else:
+            result.append(s[i])
+    
+    return "".join(result)
+
+# Example usage
+input_string = "A_B_C_D"
+output_string = restore_string(input_string)
+print(output_string)
